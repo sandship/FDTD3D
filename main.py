49ba07c378ba189389a3700533d51c14187f4369
sa-shimoyama <sa-shimoyama@kddi.com>
2019-10-04 17:21:24 +0900

    add pickle

--- main.py
+++ main.py
@@ -1,23 +1,38 @@
-import time
-import pickle
-
 from fdtdlib import emfield
 from fdtdlib import init_param
 from fdtdlib import boundary
 
+import time
+import dill
+import sys
+
+from datetime import datetime
+import logging
+
 def main():
     # load model and initialize field
-    param = init_param.InitialzeSpaceParameter()
 
-    efield = emfield.EMfield(param)
-    hfield = emfield.EMfield(param)
+    
+    if 'init' in sys.argv:
+        param = init_param.InitialzeSpaceParameter()
+        efield = emfield.EMfield(param)
+        hfield = emfield.EMfield(param)
+        with open(r"./tmp/pickle/setup_param_pickle.pkl","wb") as fb:
+            fb.write(dill.dumps(param))
+        with open(r"./tmp/pickle/setup_ef_pickle.pkl", "wb") as fb:
+            fb.write(dill.dumps(efield))
+        with open(r"./tmp/pickle/setup_hf_pickle.pkl", "wb") as fb:
+            fb.write(dill.dumps(hfield))
+    elif 'reload' in sys.argv:
+        with open(r"./tmp/pickle/setup_param_pickle.pkl", "rb") as fb:
+            param = dill.loads(fb.read())
+        with open(r"./tmp/pickle/setup_ef_pickle.pkl", "rb") as fb:
+            efield = dill.loads(fb.read())
+        with open(r"./tmp/pickle/setup_hf_pickle.pkl", "rb") as fb:
+            hfield = dill.loads(fb.read())
+    else:
+        print("You must input subcommand")
 
-    with open("param_pickle.pkl", "wb") as fb:
-        pickle.dump(param, fb)
-    with open("ef_pickle.pkl", "wb") as fb:
-        pickle.dump(efield, fb)
-    with open("hf_pickle.pkl", "wb") as fb:
-        pickle.dump(hfield, fb)
 
     # do computation
     efield.update_field()
@@ -26,8 +41,6 @@ def main():
     # result
     efield.calc_norm()
     efield.calc_phase()
-
-    print(efield.norm)
 
     return None
 
