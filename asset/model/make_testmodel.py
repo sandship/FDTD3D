949c1c32d69d27867b96c8363a740f87d3522e40
sa-shimoyama <sa-shimoyama@kddi.com>
2019-10-10 19:01:06 +0900

    add: metal

--- asset/model/make_testmodel.py
+++ asset/model/make_testmodel.py
@@ -3,5 +3,8 @@ for i in range(50):
         for k in range(50):
             if (20 < i < 30) and (20 < j < 30) and (20 < k < 30):
                 print('{} {} {} {}'.format(i, j, k, 1))
+            elif i == 10 and j == 25 and (10 < k < 40):
+                print('{} {} {} {}'.format(i, j, k, 100))
             else:            
                 print('{} {} {} {}'.format(i, j, k, 0))
+            
--- main.py
+++ main.py
@@ -11,13 +11,20 @@ def main():
     efield = emfield.Efield(param)
     hfield = emfield.Hfield(param)
 
+
+    import matplotlib.pyplot as plt
+    import seaborn as sns
     # do computation
     for _ in tqdm(range(5002)):
         efield.update_field(hfield)
         hfield.update_field(efield)
-        
-        with open('./output/dot_tl.log', "a+") as f:
-            f.write("{},{},{}\n".format(str(efield.Xaxis[50, 50, 50]), str(efield.Yaxis[50, 50, 50]), str(efield.Zaxis[50, 50, 50])))
+
+        if _ % 10 == 0:
+            efield.calc_norm()
+            plt.figure()
+            sns.heatmap(efield.norm[:, 50, :], cmap="Reds")
+            plt.savefig('./output/map_{}.png'.format(_))
+            plt.close('all')
 
     return None
 
