c15cd529c03068975046417d453c61cfcc9efe0b
sa-shimoyama <sa-shimoyama@kddi.com>
2019-10-10 17:58:52 +0900

    fix: pml boundary

--- main.py
+++ main.py
@@ -18,21 +18,6 @@ def main():
         hfield.update_field(efield)
         
         with open('./output/dot_tl.log', "a+") as f:
-            if _ % 100 == 1:
-                # result
-                efield.calc_norm()
-                efield.calc_phase()
-
-                import matplotlib.pyplot as plt
-                import seaborn as sns
-                sns.heatmap(efield.norm[50, :, :], cmap="Reds", vmin=0, vmax=0.005)
-                plt.savefig('./output/suface_{}.png'.format(_))
-                plt.close('all')
-
-                plt.plot(efield.norm[46, 46, :])
-                plt.savefig('./output/line_{}.png'.format(_))
-                plt.close('all')
-
             f.write("{},{},{}\n".format(str(efield.Xaxis[50, 50, 50]), str(efield.Yaxis[50, 50, 50]), str(efield.Zaxis[50, 50, 50])))
 
     return None
